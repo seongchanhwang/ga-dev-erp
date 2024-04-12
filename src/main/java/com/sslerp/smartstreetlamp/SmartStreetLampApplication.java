5ad17013e344c7e8fffbde134188141d7ab94a6a
seongchanhwang <tjdcksghkd12@gmail.com>
2024-04-12 12:40:54 +0900

    my first commit

---
+++ src/main/java/com/sslerp/smartstreetlamp/SmartStreetLampApplication.java
@@ -0,0 +1,13 @@
+package com.sslerp.smartstreetlamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStreetLampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStreetLampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sslerp/smartstreetlamp/SmartStreetLampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sslerp.smartstreetlamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStreetLampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
